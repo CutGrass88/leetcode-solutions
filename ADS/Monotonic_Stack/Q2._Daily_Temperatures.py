800f737a9ff082fd85eee76f10001c20ed1b0f6e
Denis Ivanciuc <238456976+CutGrass88@users.noreply.github.com>
2026-05-19 16:30:13 +0100

    Daily Temps - Med (quick using monotonic stack)

--- ADS/Monotonic_Stack/Q2._Daily_Temperatures.py
+++ ADS/Monotonic_Stack/Q2._Daily_Temperatures.py
@@ -1,17 +1,24 @@
-def dailyTemperatures(temperatues):
-    ans = [0] * len(temperatues)
+def dailyTemperatures(temperatures):
+    stack = []
+    ans = [0] * len(temperatures)
 
-    for i in range(0,len(temperatues)-1):
-        for j in range(i, len(temperatues)):
-            if i == j:
-                continue
-            if temperatues[i] < temperatues[j]:
-                ans[i] = j - i
-                break
+    for i in range(len(temperatures)):
+        if not stack:
+            stack.append(i)
+            continue
+        while stack and temperatures[stack[-1]] < temperatures[i]:
+            insertIndex = stack.pop()
+            ans[insertIndex] = i - insertIndex
+        stack.append(i)
     return ans
 
 print(dailyTemperatures([30,60,50,90]))
 # [30, 60, 50, 90]
 # [1, 2, 1, 0]
 
+#Think of a stack that maintains order via push and pops
+# push 30 index stack=[0], 60 is greater so pop 30 and at index of stack.pop = 60 index
+# 0:30 see 60 at 1 stack[1] ans = 1...
+#i=2 = 50 push 50 stack [1,2]
+#see 90
 
