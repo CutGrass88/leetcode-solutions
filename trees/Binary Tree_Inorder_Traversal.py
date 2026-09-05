b9e45bc4003e2b44fbb338cee9a3d7b24c259fd7
Denis Ivanciuc <238456976+CutGrass88@users.noreply.github.com>
2026-09-05 12:59:32 +0100

    Inorder traversal: easy

---
+++ trees/Binary Tree_Inorder_Traversal.py
@@ -0,0 +1,18 @@
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def inorderTraversal(root):
+    answer = []
+
+    def inorder(node):
+        if node is None:
+            return
+        inorder(node.left)
+        answer.append(node.val)
+        inorder(node.right)
+    inorder(root)
+    return answer
